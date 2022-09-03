7555ee7c2faa42f108d58348daf790b245d7ef30
Tully Foote <tfoote@osrfoundation.org>
2022-09-02 18:01:44 -0700

    initial commit with basic prometheus demo running
    Signed-off-by: Tully Foote <tfoote@osrfoundation.org>

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM ros:rolling
+
+
+RUN apt-get update && apt-get install -qy libcurlpp-dev libzstd-dev
+
+
+RUN git clone https://github.com/jupp0r/prometheus-cpp.git
+WORKDIR /prometheus-cpp
+RUN git submodule init && git submodule update
+
+RUN mkdir -p /prometheus-cpp/build
+WORKDIR /prometheus-cpp/build
+
+RUN cmake .. -DBUILD_SHARED_LIBS=ON -DENABLE_PUSH=ON -DENABLE_COMPRESSION=OFF
+RUN cmake --build . --parallel 4
+RUN ctest -V
+RUN cmake --install .
+
+ADD . /workspace
+WORKDIR /workspace
+RUN . /opt/ros/rolling/setup.sh && colcon build --event-handlers console_direct+
+
+CMD LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH /workspace/install/test_prometheus/lib/test_prometheus/test_prometheus
---
+++ src/test_prometheus/src/test_prometheus.cpp
@@ -0,0 +1,159 @@
+
+
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include <prometheus/counter.h>
+#include <prometheus/exposer.h>
+#include <prometheus/registry.h>
+#include <prometheus/gateway.h>
+
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+ * member function as a callback from the timer. */
+
+class MinimalPublisher : public rclcpp::Node
+{
+public:
+  MinimalPublisher()
+  : Node("minimal_publisher"), count_(0)
+  {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    timer_ = this->create_wall_timer(
+      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+     // create a metrics registry
+    // @note it's the users responsibility to keep the object alive
+    registry_ = std::make_shared<prometheus::Registry>();
+
+    // add a new counter family to the registry (families combine values with the
+    // same name, but distinct label dimensions)
+    //
+    // @note please follow the metric-naming best-practices:
+    // https://prometheus.io/docs/practices/naming/
+
+    //   auto builder = prometheus::BuildCounter();
+
+    // auto & counter = builder.Name("observed_packets_total")
+    //                          .Help("Number of observed packets")
+    //                          .Register(*registry_);
+
+
+//    auto family = prometheus::Family<prometheus::Counter> (std::string("name"), std::string("labels"), prometheus::Labels({}));
+
+    // cfam_ = std::make_shared<prometheus::Family<prometheus::Counter> > (std::string("name"), std::string("help string"), prometheus::Labels({}));
+
+
+    // const std::string& help_string = std::string("help string");
+
+
+    // registry_->Add<prometheus::Counter>(cfam_->GetName(), help_string , cfam_->GetConstantLabels());
+    
+    // cfam_ = make_shared<prometheus::Family<prometheus::Counter> >();
+    //std::string("name"), std::string("labels"), prometheus::Labels({}) );
+    
+
+//   // add and remember dimensional data, incrementing those is very cheap
+//   auto& tcp_rx_counter =
+//       packet_counter.Add({{"protocol", "tcp"}, {"direction", "rx"}});
+//   auto& tcp_tx_counter =
+//       packet_counter.Add({{"protocol", "tcp"}, {"direction", "tx"}});
+
+//   // add a counter whose dimensional data is not known at compile time
+//   // nevertheless dimensional values should only occur in low cardinality:
+//   // https://prometheus.io/docs/practices/naming/#labels
+//   auto& http_requests_counter = prometheus::BuildCounter()
+//                                     .Name("http_requests_total")
+//                                     .Help("Number of HTTP requests")
+//                                     .Register(*registry);
+
+    const auto labels = prometheus::Gateway::GetInstanceLabel("test_ros_integration_node");
+
+   gateway_ = std::make_shared<prometheus::Gateway>("127.0.0.1", "9091", "sample_client", labels);
+
+  // ask the pusher to push the metrics to the pushgateway
+  gateway_->RegisterCollectable(registry_);
+  }
+
+private:
+  void timer_callback()
+  {
+    auto message = std_msgs::msg::String();
+    message.data = "Hello, world! " + std::to_string(count_++);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+
+    // I'd like to persist this but only returning the reference makes that very hard.
+    // Requerying it seems to work.
+    auto & counter = prometheus::BuildCounter().Name("loops_total")
+                             .Help("loops packets")
+                             .Register(*registry_);
+
+    const auto random_value = std::rand();
+
+    // add and remember dimensional data, incrementing those is very cheap
+    auto& timer_counter =
+        counter.Add({{"callback_type", "timer"}});
+    auto& alt_counter =
+        counter.Add({{"callback_type", "alt"}});
+
+
+    if (random_value & 1) timer_counter.Increment();
+    if (random_value & 4) alt_counter.Increment();
+
+
+    // add a counter whose dimensional data is not known at compile time
+    // nevertheless dimensional values should only occur in low cardinality:
+    // https://prometheus.io/docs/practices/naming/#labels
+    auto& http_requests_counter = prometheus::BuildCounter()
+                                        .Name("http_requests_total")
+                                        .Help("Number of HTTP requests")
+                                        .Register(*registry_);
+
+    const std::array<std::string, 4> methods = {"GET", "PUT", "POST", "HEAD"};
+    auto method = methods.at(random_value % methods.size());
+    // dynamically calling Family<T>.Add() works but is slow and should be
+    // avoided
+    http_requests_counter.Add({{"method", method}}).Increment();
+
+    // push metrics
+    auto returnCode = gateway_->Push();
+    std::cout << "returnCode is " << returnCode << std::endl;
+
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
+  std::shared_ptr<prometheus::Registry> registry_;
+  //   std::shared_ptr<prometheus::Family<prometheus::Counter>& > loop_counter_;
+//   std::shared_ptr<prometheus::Family<prometheus::Counter> > cfam_;
+  std::shared_ptr<prometheus::Gateway> gateway_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
