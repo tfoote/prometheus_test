d705bfa6b0112ab9cd45b337bfcbb972046efcfe
Tully Foote <tfoote@osrfoundation.org>
2022-09-12 18:55:54 -0700

    Add a second timer for example statistics.
    Signed-off-by: Tully Foote <tfoote@osrfoundation.org>

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ rocker --net=host prometheustest
 
 
 Visit: http://localhost:9090 to view the output
+
+## Potential queries
+
+* `rate(callback_2_count[1m])`
+
+* `callback_1_count`
+
+* `rate(timer_callbacks_total[1m])`
+
--- src/test_prometheus/src/test_prometheus.cpp
+++ src/test_prometheus/src/test_prometheus.cpp
@@ -42,7 +42,8 @@ public:
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
-     // create a metrics registry
+    timer2_ = this->create_wall_timer(
+      750ms, std::bind(&MinimalPublisher::timer_callback2, this));     // create a metrics registry
     // @note it's the users responsibility to keep the object alive
     registry_ = std::make_shared<prometheus::Registry>();
 
@@ -65,8 +66,8 @@ private:
 
     // I'd like to persist this but only returning the reference makes that very hard.
     // Requerying it seems to work.
-    auto & counter = prometheus::BuildCounter().Name("loops_total")
-                             .Help("loops packets")
+    auto & counter = prometheus::BuildCounter().Name("callback_1_count")
+                             .Help("Callback counter")
                              .Register(*registry_);
 
     const auto random_value = std::rand();
@@ -86,15 +87,61 @@ private:
     // nevertheless dimensional values should only occur in low cardinality:
     // https://prometheus.io/docs/practices/naming/#labels
     auto& http_requests_counter = prometheus::BuildCounter()
-                                        .Name("http_requests_total")
-                                        .Help("Number of HTTP requests")
+                                        .Name("timer_callbacks_total")
+                                        .Help("Number of timer requests")
                                         .Register(*registry_);
 
-    const std::array<std::string, 4> methods = {"GET", "PUT", "POST", "HEAD"};
-    auto method = methods.at(random_value % methods.size());
+    // const std::array<std::string, 4> methods = {"GET", "PUT", "POST", "HEAD"};
+    // auto method = methods.at(random_value % methods.size());
     // dynamically calling Family<T>.Add() works but is slow and should be
     // avoided
-    http_requests_counter.Add({{"method", method}}).Increment();
+    http_requests_counter.Add({{"callback_id", "1"}}).Increment();
+
+    // push metrics
+    auto returnCode = gateway_->Push();
+    std::cout << "returnCode is " << returnCode << std::endl;
+
+  }
+
+  void timer_callback2()
+  {
+    auto message = std_msgs::msg::String();
+    message.data = "Goodbye, world! " + std::to_string(count_++);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+
+    // I'd like to persist this but only returning the reference makes that very hard.
+    // Requerying it seems to work.
+    auto & counter = prometheus::BuildCounter().Name("callback_2_count")
+                             .Help("Callback Counter")
+                             .Register(*registry_);
+
+    const auto random_value = std::rand();
+
+    // add and remember dimensional data, incrementing those is very cheap
+    auto& timer_counter =
+        counter.Add({{"callback_type", "timer"}});
+    auto& alt_counter =
+        counter.Add({{"callback_type", "alt"}});
+
+
+    if (random_value & 1) timer_counter.Increment();
+    if (random_value & 4) alt_counter.Increment();
+
+
+    // add a counter whose dimensional data is not known at compile time
+    // nevertheless dimensional values should only occur in low cardinality:
+    // https://prometheus.io/docs/practices/naming/#labels
+    auto& http_requests_counter = prometheus::BuildCounter()
+                                        .Name("timer_callbacks_total")
+                                        .Help("Number of timer requests")
+                                        .Register(*registry_);
+
+    // const std::array<std::string, 4> methods = {"GET", "PUT", "POST", "HEAD"};
+    // auto method = methods.at(random_value % methods.size());
+    // dynamically calling Family<T>.Add() works but is slow and should be
+    // avoided
+    http_requests_counter.Add({{"callback_id", "2"}}).Increment();
 
     // push metrics
     auto returnCode = gateway_->Push();
@@ -102,6 +149,7 @@ private:
 
   }
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::TimerBase::SharedPtr timer2_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   size_t count_;
   std::shared_ptr<prometheus::Registry> registry_;
